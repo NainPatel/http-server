c3a5d0cf9e20529ca04d2d9bf625fc11b6743ec1
Nen <nainpatel3305@gmail.com>
2025-08-05 21:15:59 +0530

    Implemented the mulit threading feature in server

--- main.c
+++ main.c
@@ -9,15 +9,20 @@
 #include<signal.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include<pthread.h>
 
 #define MAX 1000
 #define PORT 9090
 
-int listend,clientd;
+int listend;
 struct sockaddr_in servaddr,clientaddr;
 
+void* Thread(void*);
 
 int main(){
+	int clientd;
+	struct sockaddr_in clientaddr;
+
 	listend=socket(AF_INET,SOCK_STREAM,0);
 	if(listend<0){
 		fprintf(stderr,"Error while making socket");
@@ -49,18 +54,25 @@ int main(){
 		clientd=accept(listend,(struct sockaddr*)&clientaddr,&client_len);
 		if(clientd<0)
 			continue;
-
-		pid_t pid=fork();
-		if(fork()==0){
-			close(listend);
-			processClientRequest(clientd);
-			exit(0);
-		}
-		else if(pid>0){
-			close(clientd);
+		int* t_clientd=malloc(sizeof(int));
+		*t_clientd=clientd;
+		pthread_t thread_id;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+		if(pthread_create(&thread_id,&attr,Thread,t_clientd)){
+				perror("pthread_create");
+				return 1;
 		}
 	}
-	close(clientd);
 	return 0;
 }
 
+
+
+void* Thread(void* arg){
+	int Thread_clientd=*(int*)arg;
+	processClientRequest(Thread_clientd);
+	close(Thread_clientd);
+	return NULL;
+}
